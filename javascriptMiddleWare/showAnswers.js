3408c11c2df9581883f2bba5c8a301768724c504
2ndyrGroupA <2ndyrGroupA@pn.local>
2020-10-22 16:07:28 +0800

    Final na jud ni tawon

--- javascriptMiddleWare/showAnswers.js
+++ javascriptMiddleWare/showAnswers.js
@@ -1,17 +1,17 @@
 //Disable refreshing web page 
-// window.onbeforeunload = function(data) {
-//    return "Dude, are you sure you want to leave? Think of the kittens!";
-// }
+window.onbeforeunload = function(data) {
+   return "Dude, are you sure you want to leave? Think of the kittens!";
+}
 
 //Disable inspect
-// $(document).bind("contextmenu", function (e) {
-//     e.preventDefault();
-// });
-// $(document).keydown(function (e) {
-//     if (e.which === 123) {
-//         return false;
-//     }
-//  });
+$(document).bind("contextmenu", function (e) {
+    e.preventDefault();
+});
+$(document).keydown(function (e) {
+    if (e.which === 123) {
+        return false;
+    }
+ });
 $(document).ready(() => {
     $("#requestor").click(() => {
         $.ajax({
